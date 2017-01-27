2722cc80df8fce8d22e9fb60a4acf54fa80a41de
Di Benedetto <michele.dibenedetto@hmhco.com>
2017-01-27 17:30:28 +0000

    in progress

--- server/server.js
+++ server/server.js
@@ -39,7 +39,27 @@ router.get('/', function (req, res) {
         message: 'hooray! welcome to our api!'
     });
 });
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
 
+// create a bear (accessed at POST http://localhost:8080/api/bears)
+.post(function (req, res) {
+
+    var bear = new Bear(); // create a new instance of the Bear model
+    bear.name = req.body.name; // set the bears name (comes from the request)
+
+    // save the bear and check for errors
+    bear.save(function (err) {
+        if (err)
+            res.send(err);
+
+        res.json({
+            message: 'Bear created!'
+        });
+    });
+
+});
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
