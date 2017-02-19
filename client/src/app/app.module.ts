1ba61ba24e88229fc0f5730ed08fcafd44977873
mike <mike@micheles-MacBook-Pro.local>
2017-02-19 23:55:12 +0000

    included bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,19 @@
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { TaskListComponent } from './tasks/task-list/task-list.component';
 import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { TaskService } from './services/task.service';
+import { UserComponent } from './user/user/user.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { SettingComponent } from './setting/setting.component';
+
 
 @NgModule({
   declarations: [
@@ -15,12 +21,34 @@ import { TaskService } from './services/task.service';
     TaskComponent,
     TaskListComponent,
     TaskListItemComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    UserComponent,
+    UserListComponent,
+    SettingComponent
   ],
   imports: [
+    // NgbModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([{
+        path: '',
+        redirectTo: '/tasks',
+        pathMatch: 'full'
+      },
+      {
+        path: 'tasks',
+        component: TaskListComponent
+      },
+      {
+        path: 'users',
+        component: UserListComponent
+      },
+      {
+        path: 'settings',
+        component: SettingComponent
+      }
+    ])
   ],
   providers: [TaskService],
   bootstrap: [AppComponent]
