a1850532ec1d7e90f057e9d3c072aed3fb804b75
michele dibenedetto <michele.dibenedetto@hmhco.com>
2017-04-04 17:48:31 +0100

    routs refactorings

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,61 +6,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { TaskFormComponent } from './tasks/task-form/task-form.component';
-import { TaskListComponent } from './tasks/task-list/task-list.component';
-import { TaskListToolbarComponent } from './tasks/task-list-toolbar/task-list-toolbar.component';
-import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
 import { TaskService } from './services/task.service';
-import { UserComponent } from './user/user/user.component';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { SettingComponent } from './setting/setting.component';
-
+import { RouteDefinitions, AppRoutingModule, routingComponents } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskFormComponent,
-    TaskListComponent,
-    TaskListItemComponent,
     NavigationBarComponent,
-    UserComponent,
-    UserListComponent,
-    SettingComponent,
-    TaskListToolbarComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([{
-        path: '',
-        redirectTo: '/tasks',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tasks',
-        component: TaskListComponent
-      },
-      {
-        path: 'task',
-        component: TaskFormComponent
-      },
-      {
-        path: 'task/:id',
-        component: TaskFormComponent
-      },
-      {
-        path: 'users',
-        component: UserListComponent
-      },
-      {
-        path: 'settings',
-        component: SettingComponent
-      }
-    ])
+    AppRoutingModule
   ],
   providers: [TaskService],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ client/src/app/app.routing.ts
@@ -0,0 +1,54 @@
+import { Route, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskListToolbarComponent } from './tasks/task-list-toolbar/task-list-toolbar.component';
+import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
+import { TaskService } from './services/task.service';
+
+import { UserComponent } from './user/user/user.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { SettingComponent } from './setting/setting.component';
+
+export const RouteDefinitions: Route[] = [{
+    path: '',
+    redirectTo: '/tasks',
+    pathMatch: 'full'
+},
+{
+    path: 'tasks',
+    component: TaskListComponent
+},
+{
+    path: 'task',
+    component: TaskFormComponent
+},
+{
+    path: 'task/:id',
+    component: TaskFormComponent
+},
+{
+    path: 'users',
+    component: UserListComponent
+},
+{
+    path: 'settings',
+    component: SettingComponent
+}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(RouteDefinitions)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+export const routingComponents = [
+    TaskFormComponent,
+    TaskListComponent,
+    TaskListItemComponent,
+    UserComponent,
+    UserListComponent,
+    SettingComponent
+];
