532753e7cc582dd7a45b73f1e2241812a6684645
michele dibenedetto <michele.dibenedetto@hmhco.com>
2017-04-04 17:20:59 +0100

    test clean up

--- client/src/app/1st.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TestBed, async } from '@angular/core/testing';
-
-import { AppComponent } from './app.component';
-
-
-describe('1st tests', () => {
-  it('true is truev', () => expect(true).toBe(true));
-});
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,43 +1,42 @@
 
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app, compiled: any;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ],
+      ], 
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     });
     TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+
+    app = fixture.debugElement.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
+
   });
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'Task Manager!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Task Manager!');
-
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Task Manager!');
   }));
 
   it('should render and navigation-bar tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('navigation-bar')===null).toBeFalsy();
+    fixture.detectChanges(); 
+    expect(compiled.querySelector('navigation-bar') === null).toBeFalsy();
   }))
 });
