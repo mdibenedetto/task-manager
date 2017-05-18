2e221cbddef3215a5076ff8b09bc5984ad2ca332
michele dibenedetto <michele.dibenedetto@hmhco.com>
2017-05-18 17:15:00 +0100

    dbconnection hot fix

--- server/app/routes/task-route.js
+++ server/app/routes/task-route.js
@@ -1,20 +1,13 @@
 function load(router) {
-
     var Task = require('../models/task');
-    console.log('Task.db.readyState ' + Task.db.readyState)
-    // if (Task.db.readyState === 2) {
-    var _checkDBConnection = function (req, res) {
+
+    function raiseErrorOnConnectionClosed(req, res) {
         if (Task.db.readyState === 0)
             res.json({
                 STATUS: "DB_CONNECTION_CLOSE"
             });
     }
-    //     router.route('*')
-    //         .post(_errorDBConnection)
-    //         .get(_errorDBConnection);
-    //     return;
-    // }
-    var setTask = function (req, task) {
+    var buildTask = function (req, task) {
         var task = task || new Task();
         task.title = req.body.title;
         task.description = req.body.description;
@@ -23,21 +16,19 @@ function load(router) {
     };
     router.route('/tasks')
         .post(function (req, res) {
-            console.log(req.body);
-            var task = setTask(req, null);;
-
+            raiseErrorOnConnectionClosed(req, res);
+            var task = buildTask(req, null);;
             task.save(function (err) {
-                    err && res.send(err);
-                    res.json({
-                        status: 'ok',
-                        task: task,
-                        message: 'Task created!'
-                    });
-                },
-                _errorDBConnection);
+                err && res.send(err);
+                res.json({
+                    status: 'ok',
+                    task: task,
+                    message: 'Task created!'
+                });
+            });
         })
         .get(function (req, res) {
-            _checkDBConnection(req, res);
+            raiseErrorOnConnectionClosed(req, res);
             Task.find(function (err, tasks) {
                     err && res.send(err);
                     res.json(tasks);
@@ -47,15 +38,17 @@ function load(router) {
 
     router.route('/tasks/:_id')
         .get(function (req, res) {
+            raiseErrorOnConnectionClosed(req, res);
             Task.findById(req.params._id, function (err, task) {
                 err && res.send(err);
                 res.json(task);
             });
         })
         .put(function (req, res) {
+            raiseErrorOnConnectionClosed(req, res);
             Task.findById(req.params._id, function (err, task) {
                 err && res.send(err);
-                setTask(req, task);
+                buildTask(req, task);
                 task.save(function (err) {
                     err && res.send(err);
                     res.json({
@@ -67,14 +60,16 @@ function load(router) {
 
         })
         .delete(function (req, res) {
+            raiseErrorOnConnectionClosed(req, res);
             Task.remove({
-                _id: req.params._id
-            }, function (err, task) {
-                err && res.send(err);
-                res.json({
-                    message: 'Successfully deleted'
+                    _id: req.params._id
+                },
+                function (err, task) {
+                    err && res.send(err);
+                    res.json({
+                        message: 'Successfully deleted'
+                    });
                 });
-            });
         });
 }
 
