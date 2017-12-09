72e17079014a0864c17cbce3f9d900640376ce07
michele dibenedetto <michele.dibe78@gmail.com>
2017-12-09 18:03:05 +0900

    login css refatoring: fix with of the login component

--- client/src/app/user/login/login.component.ts
+++ client/src/app/user/login/login.component.ts
@@ -5,7 +5,8 @@ import { AuthService } from "../auth-service/auth.service";
 
 @Component({
   selector: "login",
-  templateUrl: "./login.component.html"
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
   errorMessage: string;
