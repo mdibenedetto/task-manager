d4d0744558eab44ea135bfebafb43f5a01c73ebb
michele dibenedetto <michele.dibe78@gmail.com>
2020-05-22 16:26:55 +0100

    removed .spec temporarly

--- client/src/app/modules/tasks/pages/task-filter/task-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TaskFilterPipe } from './task-filter.pipe';
-
-describe('TaskFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TaskFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
