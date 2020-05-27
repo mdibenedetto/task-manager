c56a6c200211d468e485d98785ac4743a745e90b
michele dibenedetto <michele.dibe78@gmail.com>
2020-05-27 21:53:05 +0100

    set gh-pages as not production environment

--- client/src/environments/environment.gh-pages.ts
+++ client/src/environments/environment.gh-pages.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   WEB_API_URL: "http://localhost:5000/api"
 };
 
